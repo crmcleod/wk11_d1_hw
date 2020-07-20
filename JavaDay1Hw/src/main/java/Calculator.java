b320e87dc9827478ea53b4990026640c2b0ee2d6
Craig McLeod <mcleod.craig@outlook.com>
2020-07-20 15:14:48 +0100

    Divide passing

--- JavaDay1Hw/src/main/java/Calculator.java
+++ JavaDay1Hw/src/main/java/Calculator.java
@@ -1,10 +1,14 @@
 public class Calculator {
     private int num1;
     private int num2;
+    private double num3;
+    private double num4;
 
-    public Calculator(int num1, int num2){
+    public Calculator(int num1, int num2, double num3, double num4){
         this.num1 = num1;
         this.num2 = num2;
+        this.num3 = num3;
+        this.num4 = num4;
     }
 
     public int add(){
@@ -18,4 +22,10 @@ public class Calculator {
     public int multiply(){
         return (this.num1 * this.num2);
     }
+
+    public double divide(){
+        return (this.num3 / this.num4);
+    }
+
+
 }
--- JavaDay1Hw/src/test/java/CalculatorTest.java
+++ JavaDay1Hw/src/test/java/CalculatorTest.java
@@ -7,7 +7,7 @@ public class CalculatorTest {
     private Calculator calculator;
 
     @Before
-    public void before() {this.calculator = new Calculator(5, 5);}
+    public void before() {this.calculator = new Calculator(5, 5, 6.66, 3.00);}
 
     @Test
     public void canAdd(){ assertEquals(10, calculator.add()); }
@@ -17,5 +17,8 @@ public class CalculatorTest {
 
     @Test
     public void canMultiply(){ assertEquals(25, calculator.multiply());}
+
+    @Test
+    public void canDivide(){ assertEquals(2.22, calculator.divide(), 0.01);}
 }
 
