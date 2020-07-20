baa5f6c563590ac6b4711a93dbed26b4aa21db89
Craig McLeod <mcleod.craig@outlook.com>
2020-07-20 17:09:01 +0100

    Finished

--- JavaDay1Hw/src/main/java/Calculator.java
+++ JavaDay1Hw/src/main/java/Calculator.java
@@ -8,8 +8,7 @@ public class Calculator {
     }
 
     public int subtract(int num1, int num2){
-        return (num1 - num2);
-    }
+        return (num1 - num2);}
 
     public int multiply(int num1, int num2){
         return (num1 * num2);
