03f7f5f82c799e331fcea3b3c86e96f708c2ab49
Craig McLeod <mcleod.craig@outlook.com>
2020-07-20 14:59:20 +0100

    Add and subtract passing

---
+++ JavaDay1Hw/src/test/java/CalculatorTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator;
+
+    @Before
+    public void before() {this.calculator = new Calculator(5, 5);}
+
+    @Test
+    public void canAdd(){ assertEquals(10, calculator.add()); }
+
+    @Test
+    public void canSubtract(){ assertEquals(0, calculator.subtract());}
+}
+
